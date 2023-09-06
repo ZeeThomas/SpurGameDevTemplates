c7071e38aee86c8825f6c086f407362a9cbf6d1d
Zee <missz91102@gmail.com>
2023-09-06 14:42:13 -0400

    Edited the name of function

---
+++ HelloWorldTemplate.java
@@ -0,0 +1,6 @@
+public class HelloWorldTemplate {
+    /*Hi Everyone!
+     * This is our template for our hello world file. 
+     * We're going to run through how to print "Hello World" in java. 
+     */
+}
